785390646e50eefacf38b61cb5c3d3cc3def9386
Md. Moinul Islam <49118924+moinul7002@users.noreply.github.com>
2019-12-27 10:24:43 +0600

    Add files via upload

---
+++ hotel/page_content/header_sidebar.php
@@ -0,0 +1,71 @@
+ <div class="container-scroller">
+    <!-- partial:partials/_navbar.html -->
+    <nav class="navbar col-lg-12 col-12 p-0 fixed-top d-flex flex-row default-layout-navbar">
+      
+      <div class="navbar-menu-wrapper d-flex align-items-stretch">
+        
+      
+        <ul class="navbar-nav navbar-nav-right">
+         
+          
+         
+         
+          <li class="nav-item">
+          
+            
+              
+              
+              <!--<a class="dropdown-item">-->
+              <!--  <i class="fas fa-power-off text-primary"></i>-->
+              <!--  Logout-->
+              <!--</a>-->
+            
+          </li>
+         
+        </ul>
+        <button class="navbar-toggler navbar-toggler-right d-lg-none align-self-center" type="button" data-toggle="offcanvas">
+          <span class="fas fa-bars"></span>
+        </button>
+      </div>
+    </nav>
+    <!-- partial -->
+    <div class="container-fluid page-body-wrapper">
+      <!-- partial:partials/_settings-panel.html -->
+     
+
+      <!-- partial -->
+      <!-- partial:partials/_sidebar.html -->
+      <nav class="sidebar sidebar-offcanvas" id="sidebar">
+        <ul class="nav">
+          <li class="nav-item nav-profile">
+            <div class="nav-link">
+              <div class="profile-image">
+              
+              </div>
+              <div class="profile-name">
+                <p class="name">
+                
+                </p>
+              
+              </div>
+            </div>
+          </li>
+         
+         
+         
+          
+          
+         
+         
+         
+          
+          
+        
+         
+         
+          
+         
+         
+         
+        </ul>
+      </nav>
